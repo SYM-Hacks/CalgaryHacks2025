0733cf323b89c33ea282e253b4c13826d711fcf5
Yassin Soliman <solimanyassin@gmail.com>
2025-02-15 16:26:20 -0700

    fixed error with page not displaying when running

--- app/forum/urls.py
+++ app/forum/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-
-
-from django.urls import path
-from .views import home, create_post, signup
+from django.contrib.auth import views as auth_views
+from .views import home, create_post, signup, posts_view, messaging_view
 
 urlpatterns = [
     path('', home, name='home'),
-    path('create/', create_post, name='create_post'),
     path('signup/', signup, name='signup'),
-]
+    path('create/', create_post, name='create_post'),
+    path('posts/', posts_view, name='posts'),
+    path('messaging/', messaging_view, name='messaging'),
 
+    # Authentication Routes
+    path('login/', auth_views.LoginView.as_view(template_name='forum/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+]
--- app/forum/views.py
+++ app/forum/views.py
@@ -1,5 +1,3 @@
-
-
 # Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
@@ -23,15 +21,20 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'forum/signup.html', {'form': form})
 
-def home(request):
-    posts = Post.objects.all()
-    return render(request, 'forum/home.html', {'posts': posts})
-
 @login_required
 def create_post(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
         Post.objects.create(user=request.user, title=title, content=content)
-        return redirect('/')
+        return redirect('home')  # Redirect to home instead of root
     return render(request, 'forum/create_post.html')
+
+@login_required
+def posts_view(request):
+    posts = Post.objects.all()
+    return render(request, 'forum/posts.html', {'posts': posts})
+
+@login_required
+def messaging_view(request):
+    return render(request, 'forum/messaging.html')
