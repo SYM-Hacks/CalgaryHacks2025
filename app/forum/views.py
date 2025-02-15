66bfbfd850cbfdf06557f8f7f60c65194cb40ee6
mujtaba-zia <mujtabayousufzai786@gmail.com>
2025-02-15 15:28:12 -0700

    hundle submitions, post is linked to a logged-in user

--- app/forum/views.py
+++ app/forum/views.py
@@ -1,9 +1,13 @@
-
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Post
 
-def home(request):
-    posts = Post.objects.all()
-    return render(request, 'forum/home.html', {'posts': posts})
+@login_required  # User must be logged in to submit a post
+def create_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Post.objects.create(user=request.user, title=title, content=content)
+        return redirect('home')  # Redirect to homepage after submitting
+
+    return render(request, 'forum/create_post.html')
