e428909601873da08fa089f4523361ce7ea12f5c
sam shojaei <sam1383@icloud.com>
2025-02-15 19:30:07 -0700

    added colum

--- app/forum/models.py
+++ app/forum/models.py
@@ -10,11 +10,16 @@ from django.contrib.auth.models import User
 
 def get_default_user():
     return User.objects.get_or_create(username='default_user')[0].id
-
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=get_default_user)
     title = models.CharField(max_length=255)
     content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
